c92a5c156369c69e79795a00679bd9be1392daac
Roman Karpiak <roman_karpiak@epam.com>
2021-05-25 13:52:57 +0400

    Project is builded

--- src/test/java/com/roman/controllers/CustomerControllerTest.java
+++ src/test/java/com/roman/controllers/CustomerControllerTest.java
@@ -8,6 +8,9 @@ import com.roman.entity.Customer;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.stubbing.Answer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -17,15 +20,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
 
 public class CustomerControllerTest {
 
+    @Mock
+    private CustomerDAO customerDAO;
+    @InjectMocks
+    CustomerController controller = new CustomerController();
 
-    private final List<Customer> customers = new ArrayList<>();
+
+    private List<Customer> customers = new ArrayList<>();
 
     @Before
     public void initCustomers() {
+        MockitoAnnotations.openMocks(this);
         Customer customerOne = new Customer();
         customerOne.setId(1L);
         customerOne.setName("Alex");
@@ -46,112 +56,73 @@ public class CustomerControllerTest {
     }
 
     @Test
-    public void testList(){
-        CustomerDAO customerDAO = mock(CustomerDAO.class);
+    public void getListCustomersTest() {
         when(customerDAO.findAll()).thenReturn(customers);
-
-        CustomerController controller = new CustomerController();
 
         ReflectionTestUtils.setField(controller, "customerDAO", customerDAO);
         ExtendedModelMap uiModel = new ExtendedModelMap();
         uiModel.addAttribute("customers", controller.getListCustomers());
         ResponseEntity<List<Customer>> model = (ResponseEntity<List<Customer>>) uiModel.get("customers");
+
         Assert.assertEquals(200, model.getStatusCodeValue());
         Assert.assertEquals(2, Objects.requireNonNull(model.getBody()).size());
     }
 
 
-    @Test
-    public void getCustomerById() {
-        CustomerDAO customerDAO = mock(CustomerDAO.class);
+    @Test()
+    public void getCustomerById()  {
         when(customerDAO.findById(2L)).thenReturn(customers.get(1));
-
-        CustomerController controller = new CustomerController();
 
         ReflectionTestUtils.setField(controller, "customerDAO", customerDAO);
         ExtendedModelMap uiModel = new ExtendedModelMap();
         uiModel.addAttribute("customer", controller.getCustomerById(2L));
-        ResponseEntity<Customer> model = (ResponseEntity<Customer>) uiModel.get("customer");
+        ResponseEntity<Customer> model = (ResponseEntity<Customer>)uiModel.get("customer");
+
         Assert.assertEquals(200, model.getStatusCodeValue());
         Assert.assertEquals("Sergey", Objects.requireNonNull(model.getBody()).getName());
     }
 
     @Test
     public void whenCreateCustomerGetStatus201() {
-        final Customer customer = new Customer();
+        Customer customer = new Customer();
         customer.setId(2L);
         customer.setName("Fedor");
         customer.setPhone("111");
-        customer.setEmail("alex@sd");
+        customer.setEmail("fedor@sd");
         customer.setCart(new Cart());
         customer.setAddress(new Address("Russia", "Moscow", "New"));
-        CustomerDAO customerDAO = mock(CustomerDAO.class);
+
         doAnswer((Answer<Customer>) invocationOnMock -> {
             customers.add(customer);
             return customer;
         }).when(customerDAO).create(customer);
 
-
-        CustomerController controller = new CustomerController();
         ReflectionTestUtils.setField(controller, "customerDAO", customerDAO);
         ResponseEntity<Customer> responseEntity = controller.createCustomer(customer);
+
         Assert.assertEquals(201, responseEntity.getStatusCodeValue());
         Assert.assertEquals("Fedor", Objects.requireNonNull(responseEntity.getBody()).getName());
     }
 
-//    @Test
-//    void whenUpdateCustomerNameThenReturnStatus200AndUpdateCustomer() {
-//        Customer customer = new Customer();
-//        customer.setId(1L);
-//        customer.setName("Alex");
-//        customer.setPhone("111");
-//        customer.setEmail("alex@sd");
-//        customer.setCart(new Cart());
-//        customer.setAddress(new Address("Russia", "Moscow", "New"));
-//
-//        Customer customerUpdate = new Customer();
-//        customerUpdate.setId(1L);
-//        customerUpdate.setName("Fedor");
-//        customerUpdate.setPhone("111");
-//        customerUpdate.setEmail("alex@sd");
-//        customerUpdate.setCart(new Cart());
-//        customerUpdate.setAddress(new Address("Russia", "Moscow", "New"));
-//        controller.createCustomer(customer);
-//        controller.createCustomer(customer);
-//
-//        CustomerController controller = Mockito.mock(CustomerController.class);
-//        doReturn(new ResponseEntity<>(customerUpdate, HttpStatus.OK)).when(controller).update(1L, customerUpdate);
-//
-//        ResponseEntity<Customer> responseEntity = controller.update(1L, customerUpdate);
-//        assertEquals(200, responseEntity.getStatusCodeValue());
-//        assertEquals("Fedor", Objects.requireNonNull(responseEntity.getBody()).getName());
-//    }
-//    @Test
-//    void whenUpdateCustomerNameAndCustomerNotFoundThenReturnStatus304() {
-//
-//        Customer customer = new Customer();
-//        customer.setId(1L);
-//        customer.setName("Alex");
-//        customer.setPhone("111");
-//        customer.setEmail("alex@sd");
-//        customer.setCart(new Cart());
-//        customer.setAddress(new Address("Russia", "Moscow", "New"));
-//
-//        Customer customerUpdate = new Customer();
-//        customerUpdate.setId(1L);
-//        customerUpdate.setName("Fedor");
-//        customerUpdate.setPhone("111");
-//        customerUpdate.setEmail("alex@sd");
-//        customerUpdate.setCart(new Cart());
-//        customerUpdate.setAddress(new Address("Russia", "Moscow", "New"));
-//
-//        CustomerController controller = Mockito.mock(CustomerController.class);
-//        doReturn(new ResponseEntity<>(HttpStatus.NOT_MODIFIED)).when(controller).update(5L, customerUpdate);
-//        controller.createCustomer(customer);
-//        controller.update(5L,customerUpdate);
-//        ResponseEntity<Customer> responseEntityNotUpdate = controller.update(5L, customerUpdate);
-//        assertEquals(304, responseEntityNotUpdate.getStatusCodeValue());
-//
-//    }
+    @Test
+    public void whenUpdateCustomerNameThenReturnStatus200AndUpdateCustomer() {
+        Customer updatedCustomer1 = new Customer();
+        updatedCustomer1.setId(1L);
+        updatedCustomer1.setName("Leonid");
+        updatedCustomer1.setPhone("555555");
+        updatedCustomer1.setEmail("leonid@sd");
+        updatedCustomer1.setCart(new Cart());
+        updatedCustomer1.setAddress(new Address("Russia", "Moscow", "New"));
+
+        when(customerDAO.findById(1L)).thenReturn(customers.get(0));
+
+        ReflectionTestUtils.setField(controller, "customerDAO", customerDAO);
+
+        ResponseEntity<Customer> responseEntity = controller.update(1L, updatedCustomer1);
+        Assert.assertEquals(200, responseEntity.getStatusCodeValue());
+        Assert.assertEquals("Leonid", Objects.requireNonNull(responseEntity.getBody()).getName());
+        Assert.assertEquals("leonid@sd", Objects.requireNonNull(responseEntity.getBody()).getEmail());
+        Assert.assertEquals("555555", Objects.requireNonNull(responseEntity.getBody()).getPhone());
+    }
 
 }
